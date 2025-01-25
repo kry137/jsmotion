2b9f39171dfa84089c27ac79e19434caa68072ef
= <=>
2025-01-25 12:28:43 +0700

    Update Follow-Mouse
    Sekarang elemen yang mengejar mouse bisa lebih dari 1

--- Follow-Mouse/follow-mouse.js
+++ Follow-Mouse/follow-mouse.js
@@ -1,8 +1,8 @@
 let animationSpeed = 0.04; // Kecepatan gerak layer terdepan
 let animationSpeedOther = 0.5; // Kecepatan gerak layer belakang
 
-const container = document.getElementById("container-follow-mouse");
-const children = Array.from(container.children);
+const containers = Array.from(document.getElementsByClassName("container-follow-mouse"));
+// const children = Array.from(container.children);
 
 let lastMousex = 0;
 let lastMousey = 0;
@@ -12,15 +12,14 @@ let lastMousey = 0;
 document.addEventListener("mousemove", function (mouse) {
   lastMousex = mouse.x;
   lastMousey = mouse.y;
-  // console.log(lastMousex);
-  // console.log(lastMousey);
+  // console.log(lastMousex); console.log(lastMousey);
 });
 
 // Parse Float, return 0 kalau NaN
 function getOffset(element, direc) {
   let data = (direc == 0) ?
-  element.getAttribute('data-offset-x') : 
-  element.getAttribute('data-offset-y');
+    element.getAttribute('data-offset-x') :
+    element.getAttribute('data-offset-y');
   return (data !== null) ? data : 0;
 }
 
@@ -29,34 +28,50 @@ requestAnimationFrame(EachFrameLoop);
 
 function EachFrameLoop() {
   requestAnimationFrame(function () {
-    let childBefore; // Elemen anak terakhir yang udah kepakai disimpan sementara disini
-    for (let i = children.length; i > 0; i--) {
-      let child = Array.from(children)[i - 1];
+    containers.forEach((container) => {
 
-      // Menentukan arah dan kecepatan gerak
-      let childBeforeDefined = (childBefore !== undefined);
-      let referencex = (childBeforeDefined) ? getOffset(childBefore, 0) : lastMousex;
-      let referencey = (childBeforeDefined) ? getOffset(childBefore, 1) : lastMousey;
-      let speed = (childBeforeDefined) ? animationSpeedOther : animationSpeed;
-      
-      // Dapatkan selisih antara posisi elemen dengan mouse
-      let posisix = getOffset(child, 0);
-      let posisiy = getOffset(child, 1);
-      let selisihx = posisix - referencex;
-      let selisihy = posisiy - referencey;
+      const children = Array.from(container.children);
 
-      let posx = posisix - selisihx * speed;
-      let posy = posisiy - selisihy * speed;
+      let childBefore; // Elemen anak terakhir yang sudah terpakai akan disimpan sementara disini
 
-      // Gerakkan
-      child.style.transform = `translate(calc(-50% + ${posx + "px"}), calc(-50% + ${posy + "px"}))`;
+      // Ambil informasi kecepatan yang ada di elemen induk
+      let defmotionSpd = (container.getAttribute('data-flwmouse-defspeed') !== null) ?
+        container.getAttribute('data-flwmouse-defspeed') : 0;
+      let motionSpd = (container.getAttribute('data-flwmouse-speed') !== null) ?
+        container.getAttribute('data-flwmouse-speed') : 0;
 
-      // Reparasi dan persiapan berikutnya
-      child.setAttribute('data-offset-x', posx);
-      child.setAttribute('data-offset-y', posy);
-      childBefore = child;
-    }
+      for (let i = children.length; i > 0; i--) {
+        let child = Array.from(children)[i - 1];
+
+        // // Mengambil kecepatan dari data yang disimpan di atribut
+        // const motionSpd = child
+
+        // Menentukan arah dan kecepatan gerak
+        let childBeforeDefined = (childBefore !== undefined);
+        let referencex = (childBeforeDefined) ? getOffset(childBefore, 0) : lastMousex;
+        let referencey = (childBeforeDefined) ? getOffset(childBefore, 1) : lastMousey;
+        let speed = (childBeforeDefined) ? motionSpd : defmotionSpd;
+
+        // Dapatkan selisih antara posisi elemen dengan mouse
+        let posisix = getOffset(child, 0);
+        let posisiy = getOffset(child, 1);
+        let selisihx = posisix - referencex;
+        let selisihy = posisiy - referencey;
+
+        let posx = posisix - selisihx * speed;
+        let posy = posisiy - selisihy * speed;
+
+        // Gerakkan
+        child.style.transform = `translate(calc(-50% + ${posx + "px"}), calc(-50% + ${posy + "px"}))`;
+
+        // Reparasi dan persiapan berikutnya
+        child.setAttribute('data-offset-x', posx);
+        child.setAttribute('data-offset-y', posy);
+        childBefore = child;
+      }
+    });
   });
 
+  // Jalankan function ini lagi di frame berikutnya
   requestAnimationFrame(EachFrameLoop);
 }
